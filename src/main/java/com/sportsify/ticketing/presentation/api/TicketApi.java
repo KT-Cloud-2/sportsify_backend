682022c8e95120c8abe225b0df7aa2d037b9faa0
glosona <sonaze28@gmail.com>
2026-05-20 13:08:40 +0900

    docs: Modify erd and api, Add Swagger
    - Game, Ticketing 도메인 수정
    - ERD, API 문서 최신화 반영
    - Swagger 반영

---
+++ src/main/java/com/sportsify/ticketing/presentation/api/TicketApi.java
@@ -0,0 +1,26 @@
+package com.sportsify.ticketing.presentation.api;
+
+import com.sportsify.common.swagger.AuthRequiredApi;
+import com.sportsify.common.swagger.CommonApiResponses;
+import com.sportsify.common.swagger.SwaggerApi;
+import com.sportsify.ticketing.presentation.dto.TicketListResponseDto;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Tag(name = "티켓", description = "티켓 조회 API")
+@AuthRequiredApi
+@CommonApiResponses
+public interface TicketApi {
+
+    @SwaggerApi(summary = "내 예매 내역 조회",
+            description = "로그인한 회원의 티켓 목록을 페이지네이션으로 조회합니다. page/size 파라미터로 제어합니다.")
+    ResponseEntity<TicketListResponseDto> getMyTickets(
+            Long memberId,
+            @Parameter(description = "페이지 번호 (0부터 시작, 기본 0)")
+            @RequestParam(defaultValue = "0") int page,
+            @Parameter(description = "페이지 크기 (기본 10, 최대 100)")
+            @RequestParam(defaultValue = "10") int size
+    );
+}
